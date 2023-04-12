320d6da8cb4db6c2e0d1d09871081de05afd569f
Papsan <papsanlysenko@gmail.com>
2023-04-13 01:34:27 +0300

    separated all used colors into colors.js

--- src/colors.js
+++ src/colors.js
@@ -1,4 +1,10 @@
 export const colors = {
-  background: '#a091ce',
-  textDark: '#36395a'
+  backgroundDark: '#a091ce',
+  backgroundLighter: '#d8cef1',
+  backgroundLight: '#f1eeff',
+  textDark: '#36395a',
+  textLight: '#8282ba',
+  textWarning: '#b3287b',
+  buttonPink: '#d074d7',
+  buttonPurple: '#7e74d7'
 }
