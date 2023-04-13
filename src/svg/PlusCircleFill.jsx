625cd552c9788b5cfe305f011eadea5bab2de2b4
Papsan <papsanlysenko@gmail.com>
2023-04-13 20:31:26 +0300

    added icons svg files

---
+++ src/svg/PlusCircleFill.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Svg, { Path } from 'react-native-svg'
+
+const PlusCircleFill = props => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    fill="white"
+    viewBox={'0 0 16 16'}
+    {...props}
+  >
+    <Path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+  </Svg>
+)
+export default PlusCircleFill
